27d5c75db8006192bac1ef69ceeefc7f35398b30
David Schoenholtz <dmschoenholtz@gmail.com>
2019-09-28 20:27:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 function findMatching(drivers, name){
   return drivers.filter(function(driver){
-    return (driver.name === name);
+    return (driver.name === name || driver === name.toLowerCase());
   })
 }
